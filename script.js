4cc8eb752f779edf5476b2f6382e6bee0980c662
Túlio Scarlatelli <scarlatellitulio@gmail.com>
2024-08-12 16:38:46 -0300

    Código desenvolvido

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 /* Desenvolva seu código abaixo */
+function playRockPaperScissor (player1, player2) {
+
+    let resultado = "";
+  
+    if (player1 === "Pedra" && player2 === "Tesoura"){
+      
+      resultado = "Jogador 1 venceu!";
+  
+    } else if (player1 === "Tesoura" && player2 === "Papel"){
+  
+      resultado = "Jogador 1 venceu!";
+  
+    } else if (player1 === "Papel" && player2 === "Pedra"){
+  
+      resultado = "Jogador 1 venceu!";
+  
+    } else if (player2 === "Pedra" && player1 === "Tesoura"){
+      
+      resultado = "Jogador 2 venceu!";
+  
+    } else if (player2 === "Tesoura" && player1 === "Papel"){
+  
+      resultado = "Jogador 2 venceu!";
+  
+    } else if (player2 === "Papel" && player1 === "Pedra"){
+  
+      resultado = "Jogador 2 venceu!";
+    
+    } else if (player1 === player2){
+  
+      resultado = "Empate!";
+  
+    }
+  
+    return resultado;
+  
+  }
+  
+  console.log(playRockPaperScissor("Tesoura", "Tesoura"))
