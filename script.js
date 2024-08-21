275a46bfef2995d2268fc3e38aae188ca00788da
Túlio Scarlatelli <scarlatellitulio@gmail.com>
2024-08-21 19:31:45 -0300

    Correção

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ function playRockPaperScissor (player1, player2) {
     let resultado = "";
   
     if (
-    (player1 === "Pedra" && player2 === "Tesoura") ||
-    (player1 === "Tesoura" && player2 === "Papel") ||
-    (player1 === "Papel" && player2 === "Pedra")
+      (player1 === "Pedra" && player2 === "Tesoura") ||
+      (player1 === "Tesoura" && player2 === "Papel") ||
+      (player1 === "Papel" && player2 === "Pedra")
     ){
 
       resultado = "Jogador 1 venceu!";
   
     } else if (
-    (player2 === "Pedra" && player1 === "Tesoura") ||
-    (player2 === "Tesoura" && player1 === "Papel") || 
-    (player2 === "Papel" && player1 === "Pedra")
+      (player2 === "Pedra" && player1 === "Tesoura") ||
+      (player2 === "Tesoura" && player1 === "Papel") || 
+      (player2 === "Papel" && player1 === "Pedra")
     ){
   
       resultado = "Jogador 2 venceu!";
